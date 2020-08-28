ee0b335d8c1690d40e80160635a3cdf7f623fdc7
minhduc1612112 <duc1612112@gmail.com>
2020-08-28 21:44:34 +0700

    Updates

---
+++ precache-manifest.142019bc748eb2476782246e80110da4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79bc37219ac7c7c8c848c2b2db7fee03",
+    "url": "/Advanced-web-application-development--Internet-banking-customer/index.html"
+  },
+  {
+    "revision": "0ccfb9a87059123d1495",
+    "url": "/Advanced-web-application-development--Internet-banking-customer/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "be3d8d13da0b83578a56",
+    "url": "/Advanced-web-application-development--Internet-banking-customer/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "0ccfb9a87059123d1495",
+    "url": "/Advanced-web-application-development--Internet-banking-customer/static/js/2.ef0ac93f.chunk.js"
+  },
+  {
+    "revision": "e9b70ed94c92560626f6d789c6814c32",
+    "url": "/Advanced-web-application-development--Internet-banking-customer/static/js/2.ef0ac93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3d8d13da0b83578a56",
+    "url": "/Advanced-web-application-development--Internet-banking-customer/static/js/main.daa4cf9b.chunk.js"
+  },
+  {
+    "revision": "8535525b9a5fd30bf734",
+    "url": "/Advanced-web-application-development--Internet-banking-customer/static/js/runtime-main.0f7e36c2.js"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/Advanced-web-application-development--Internet-banking-customer/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/Advanced-web-application-development--Internet-banking-customer/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/Advanced-web-application-development--Internet-banking-customer/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/Advanced-web-application-development--Internet-banking-customer/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
